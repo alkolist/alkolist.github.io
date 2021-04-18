bc18cc462e230606bdf9f605b742ae605becbb26
C4illin <emrik.ostling@gmail.com>
2021-04-19 00:25:11 +0200

    changed font

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 function filterTable(columnNumber, input) {
   let filter = input.value.toLowerCase()
   let table = document.getElementById("apk-table")
@@ -24,18 +25,20 @@ fetch('https://raw.githubusercontent.com/C4illin/systembolaget-data/main/apksort
     let table = document.getElementById("apk-table")
     let tableHeader = document.createElement("tr")
     
+    tableHeader.appendChild(document.createElement("th")).textContent = ""
     tableHeader.appendChild(document.createElement("th")).textContent = "APK"
     tableHeader.appendChild(document.createElement("th")).textContent = "Namn"
     tableHeader.appendChild(document.createElement("th")).textContent = "Typ"
     tableHeader.appendChild(document.createElement("th")).textContent = "Alkohol"
     tableHeader.appendChild(document.createElement("th")).textContent = "Volym"
     tableHeader.appendChild(document.createElement("th")).textContent = "Pris"
-    tableHeader.appendChild(document.createElement("th")).textContent = "Rank"
+    
 
     table.appendChild(tableHeader)
     for (let num in data) {
       let tablerow = document.createElement("tr")
 
+      tablerow.appendChild(document.createElement("td")).innerHTML = "<b>" + (Number(num) + 1) + "</b>"
       tablerow.appendChild(document.createElement("td")).textContent = data[num]["apk"]
       if (data[num]["subtitle"]) {
         tablerow.appendChild(document.createElement("td")).innerHTML = "<a href='https://www.systembolaget.se/produkt/" + data[num]["url"] + "/'><b>" + data[num]["name"] + "</b> " + data[num]["subtitle"] + "</a>"
@@ -46,8 +49,6 @@ fetch('https://raw.githubusercontent.com/C4illin/systembolaget-data/main/apksort
       tablerow.appendChild(document.createElement("td")).textContent = data[num]["alcohol"]
       tablerow.appendChild(document.createElement("td")).textContent = data[num]["volume"].replace(" ", String.fromCharCode(160))
       tablerow.appendChild(document.createElement("td")).textContent = String(data[num]["price"]).replace(".", ",") + " kr"
-      tablerow.appendChild(document.createElement("td")).textContent = Number(num) + 1
-      
 
       table.appendChild(tablerow)
     }
