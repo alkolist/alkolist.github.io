42e4f0d52799562726cad7221757b920ff0e7247
C4illin <emrik.ostling@gmail.com>
2021-05-08 16:03:07 +0200

    moved to Jekyll

--- script.js
+++ script.js
@@ -1,55 +1,49 @@
-// eslint-disable-next-line no-unused-vars
-function filterTable(columnNumber, input) {
-  let filter = input.value.toLowerCase()
-  let table = document.getElementById("apk-table")
-  let tr = table.getElementsByTagName("tr")
+---
 
-  // Loop through all table rows, and hide those who don't match the search query
-  for (let i = 0; i < tr.length; i++) {
-    let td = tr[i].getElementsByTagName("td")[columnNumber]
-    if (td) {
-      let txtValue = td.textContent
-      if (txtValue.toLowerCase().indexOf(filter) > -1) {
-        tr[i].style.display = ""
-      } else {
-        tr[i].style.display = "none"
-      }
-    }
-  }
+---
+
+var rows = [{% for row in site.data.apk %}{% if forloop.first != true %},{% endif %}"<tr><td>{{forloop.index}}</td><td>{{row['apk'] | replace: '.', ','}}</td><td><a href='https://www.systembolaget.se/produkt/{{row['url']}}/' target='_blank'><b>{{row['name'] | replace: '"', '\"'}}</b>{{' ' | append: row['subtitle'] | replace: '"', '\"'}}</a></td><td>{{row['tags'] | join: ', '}}</td><td>{{row['alcohol'] | replace: ' ', ' '}}</td><td>{{row['volume'] | replace: ' ', ' '}}</td><td>{{row['price'] | replace: '.', ',' | append: ' kr'}}</td></tr>"{% endfor %}];
+
+var tags = [{% for row in site.data.apk %}{% if forloop.first != true %},{% endif %}"{{row['tags'] | join: ','}}"{% endfor %}];
+
+var active = []
+for (var i = 0, ii = rows.length; i < ii; i++) {
+  active.push(true)
 }
 
-fetch('https://raw.githubusercontent.com/C4illin/systembolaget-data/main/apksort.json')
-  .then(response => response.json())
-  .then(data => {
-    // console.log(data)
-    let table = document.getElementById("apk-table")
-    let tableHeader = document.createElement("tr")
-    
-    tableHeader.appendChild(document.createElement("th")).textContent = ""
-    tableHeader.appendChild(document.createElement("th")).textContent = "APK"
-    tableHeader.appendChild(document.createElement("th")).textContent = "Namn"
-    tableHeader.appendChild(document.createElement("th")).textContent = "Typ"
-    tableHeader.appendChild(document.createElement("th")).textContent = "Alkohol"
-    tableHeader.appendChild(document.createElement("th")).textContent = "Volym"
-    tableHeader.appendChild(document.createElement("th")).textContent = "Pris"
-    
+var search = document.getElementById('type-search');
 
-    table.appendChild(tableHeader)
-    for (let num in data) {
-      let tablerow = document.createElement("tr")
-
-      tablerow.appendChild(document.createElement("td")).innerHTML = "<b>" + (Number(num) + 1) + "</b>"
-      tablerow.appendChild(document.createElement("td")).textContent = data[num]["apk"]
-      if (data[num]["subtitle"]) {
-        tablerow.appendChild(document.createElement("td")).innerHTML = "<a href='https://www.systembolaget.se/produkt/" + data[num]["url"] + "/' target='_blank'><b>" + data[num]["name"] + "</b> " + data[num]["subtitle"] + "</a>"
-      } else {
-        tablerow.appendChild(document.createElement("td")).innerHTML = "<a href='https://www.systembolaget.se/produkt/" + data[num]["url"] + "/' target='_blank'><b>" + data[num]["name"] + "</b></a>"
-      }
-      tablerow.appendChild(document.createElement("td")).textContent = data[num]["tags"].join(", ")
-      tablerow.appendChild(document.createElement("td")).textContent = data[num]["alcohol"]
-      tablerow.appendChild(document.createElement("td")).textContent = data[num]["volume"].replace(" ", String.fromCharCode(160))
-      tablerow.appendChild(document.createElement("td")).textContent = String(data[num]["price"]).replace(".", ",") + " kr"
-
-      table.appendChild(tablerow)
+var filterRows = function(rows) {
+  var results = [];
+  for (var i = 0, ii = rows.length; i < ii; i++) {
+    if (active[i]) {
+      results.push(rows[i])
     }
-  })
+  }
+  return results;
+}
+
+var clusterize = new Clusterize({
+  rows: filterRows(rows),
+  scrollId: 'scrollArea',
+  contentId: 'contentArea'
+})
+
+var onSearch = function() {
+  for(var i = 0, ii = rows.length; i < ii; i++) {
+    var suitable = false;
+    if(tags[i].includes(search.value)) {
+      suitable = true;
+    }
+    active[i] = suitable;
+  }
+  clusterize.update(filterRows(rows));
+}
+
+let timeout = null;
+search.addEventListener('keyup', function (e) {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      onSearch()
+    }, 1000);
+});
