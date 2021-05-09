884a62300e6a9d65fba35dc8c81cc16dc2faebff
C4illin <emrik.ostling@gmail.com>
2021-05-09 23:03:05 +0200

    added name search

--- script.js
+++ script.js
@@ -2,25 +2,28 @@
 
 ---
 
-var rows = [{% for row in site.data.apk %}{% if forloop.first != true %},{% endif %}"<tr><td>{{forloop.index}}</td><td>{{row['apk'] | replace: '.', ','}}</td><td><a href='https://www.systembolaget.se/produkt/{{row['url']}}/' target='_blank'><b>{{row['name'] | replace: '"', '\"'}}</b>{{' ' | append: row['subtitle'] | replace: '"', '\"'}}</a></td><td>{{row['tags'] | join: ', '}}</td><td>{{row['alcohol'] | replace: ' ', ' '}}</td><td>{{row['volume'] | replace: ' ', ' '}}</td><td>{{row['price'] | replace: '.', ',' | append: ' kr'}}</td></tr>"{% endfor %}];
+var rows = [{% for row in site.data.apk %}{% if forloop.first != true %},{% endif %}"<tr><td>{{forloop.index}}</td><td>{{row['apk'] | replace: '.', ','}}</td><td><a href='https://www.systembolaget.se/produkt/{{row['url']}}/' target='_blank'><b>{{row['name'] | replace: '"', '\"'}}</b>{{' ' | append: row['subtitle'] | replace: '"', '\"'}}</a></td><td>{{row['tags'] | join: ', '}}</td><td>{{row['alcohol'] | replace: ' ', ' '}}</td><td>{{row['volume'] | replace: ' ', ' '}}</td><td>{{row['price'] | replace: '.', ',' | append: ' kr'}}</td></tr>"{% endfor %}]
 
-var tags = [{% for row in site.data.apk %}{% if forloop.first != true %},{% endif %}"{{row['tags'] | join: ','}}"{% endfor %}];
+var tags = [{% for row in site.data.apk %}{% if forloop.first != true %},{% endif %}"{{row['tags'] | join: ','}}"{% endfor %}]
+
+var names = [{% for row in site.data.apk %}{% if forloop.first != true %},{% endif %}"{{row['name'] | append: ' ' | append: row['subtitle'] | downcase | replace: '"', '\"'}}"{% endfor %}]
 
 var active = []
 for (var i = 0, ii = rows.length; i < ii; i++) {
   active.push(true)
 }
 
-var search = document.getElementById('type-search');
+var typeSearch = document.getElementById('type-search')
+var nameSearch = document.getElementById('name-search')
 
 var filterRows = function(rows) {
-  var results = [];
+  var results = []
   for (var i = 0, ii = rows.length; i < ii; i++) {
     if (active[i]) {
       results.push(rows[i])
     }
   }
-  return results;
+  return results
 }
 
 var clusterize = new Clusterize({
@@ -30,20 +33,35 @@ var clusterize = new Clusterize({
 })
 
 var onSearch = function() {
+  console.log(typeSearch.value)
+  console.log(nameSearch.value)
   for(var i = 0, ii = rows.length; i < ii; i++) {
-    var suitable = false;
-    if(tags[i].includes(search.value)) {
-      suitable = true;
+    var suitable = false
+    if(tags[i].includes(typeSearch.value) && names[i].includes(nameSearch.value)) {
+      suitable = true
     }
-    active[i] = suitable;
+    active[i] = suitable
   }
-  clusterize.update(filterRows(rows));
+  clusterize.update(filterRows(rows))
 }
 
-let timeout = null;
-search.addEventListener('keyup', function (e) {
-    clearTimeout(timeout);
-    timeout = setTimeout(function () {
-      onSearch()
-    }, 500);
-});
+let timeout = null
+typeSearch.addEventListener('keyup', function() {
+  clearTimeout(timeout)
+  timeout = setTimeout(function () {
+    onSearch()
+  }, 500)
+})
+
+nameSearch.addEventListener('keyup', function() {
+  clearTimeout(timeout)
+  timeout = setTimeout(function () {
+    onSearch()
+  }, 500)
+})
+
+let clearSearchBox = () => {
+  typeSearch.value = ""
+  nameSearch.value = ""
+  clusterize.update(rows)
+}
