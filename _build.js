cd9f0d57c4aed0a66c26bad0befaa573610cc07b
Emrik Östling <emrik.ostling@gmail.com>
2022-10-07 15:05:55 +0200

    Update _build.js

--- _build.js
+++ _build.js
@@ -2,7 +2,7 @@ const https = require("https")
 const fs = require("fs")
 const core = require("@actions/core") // required to be able to fail correctly
 
-let url = "https://raw.githubusercontent.com/C4illin/systembolaget-data/main/products.json"
+let url = "https://susbolaget.emrik.org/v1/products"
 
 // uses https instaed of fetch to bring down the amount of dependencies
 https.get(url, (res) => {
