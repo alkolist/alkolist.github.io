7eb6653908d6f0b090984d7592d5f5f1580fc911
Emrik Östling <emrik.ostling@gmail.com>
2026-08-11 21:09:50 +0200

    fix for new version of js-yaml

--- _build.js
+++ _build.js
@@ -1,90 +1,90 @@
-import https from "node:https";
-import fs from "node:fs";
-import yaml from "js-yaml";
 import * as core from "@actions/core"; // required to be able to fail correctly
+import * as yaml from "js-yaml";
+import fs from "node:fs";
+import https from "node:https";
 
 const url = "https://susbolaget.emrik.org/v1/products";
 
 // uses https instaed of fetch to bring down the amount of dependencies
 https
-	.get(url, (res) => {
-		let body = "";
-		res.on("data", (chunk) => {
-			body += chunk;
-		});
+  .get(url, (res) => {
+    let body = "";
+    res.on("data", (chunk) => {
+      body += chunk;
+    });
 
-		res.on("end", () => {
-			try {
-				let json;
+    res.on("end", () => {
+      try {
+        let json;
 
-				try {
-					json = JSON.parse(body);
-				} catch (error) {
-					// exit and don't change the website if error is detected
-					core.setFailed(error.message);
-				}
+        try {
+          json = JSON.parse(body);
+        } catch (error) {
+          // exit and don't change the website if error is detected
+          core.setFailed(error.message);
+        }
 
-				if (!json.length || json.length < 100) {
-					core.setFailed("No products found, backend is probably down");
-				}
-				console.log(`Found ${json.length} products`);
+        if (!json.length || json.length < 100) {
+          core.setFailed("No products found, backend is probably down");
+        }
+        console.log(`Found ${json.length} products`);
 
-				for (let i = 0; i < json.length; i++) {
-					if (json[i].alcoholPercentage == null) {
-						json[i].alcoholPercentage = 0;
-					}
+        for (let i = 0; i < json.length; i++) {
+          if (json[i].alcoholPercentage == null) {
+            json[i].alcoholPercentage = 0;
+          }
 
-					let apk =
-						(json[i].alcoholPercentage * json[i].volume) / json[i].price / 100;
+          let apk =
+            (json[i].alcoholPercentage * json[i].volume) / json[i].price / 100;
 
-					// happens when divided by zero and such
-					if (apk == null) {
-						apk = 0;
-					}
+          // happens when divided by zero and such
+          if (apk == null) {
+            apk = 0;
+          }
 
-					// compress all tags to one
-					json[i].tags = [
-						json[i].categoryLevel1,
-						json[i].categoryLevel2,
-						json[i].categoryLevel3,
-						json[i].categoryLevel4,
-					].filter((n) => n);
+          // compress all tags to one
+          json[i].tags = [
+            json[i].categoryLevel1,
+            json[i].categoryLevel2,
+            json[i].categoryLevel3,
+            json[i].categoryLevel4,
+          ].filter((n) => n);
 
-					if (json[i].assortmentText === "Ordervaror") {
-						json[i].tags.push("Ordervara");
-					}
+          if (json[i].assortmentText === "Ordervaror") {
+            json[i].tags.push("Ordervara");
+          }
 
-					if (json[i].assortmentText === "Webblanseringar") {
-						json[i].tags.push("Webblansering");
-					}
+          if (json[i].assortmentText === "Webblanseringar") {
+            json[i].tags.push("Webblansering");
+          }
 
-					// json[i].isTemporaryOutOfStock ||
-					if (json[i].isCompletelyOutOfStock || json[i].isDiscontinued) {
-						json[i].tags.push("Slut i lager");
-					}
+          // json[i].isTemporaryOutOfStock ||
+          if (json[i].isCompletelyOutOfStock || json[i].isDiscontinued) {
+            json[i].tags.push("Slut i lager");
+          }
 
-					if (json[i].isNews) {
-						json[i].tags.push("Nyhet");
-					}
+          if (json[i].isNews) {
+            json[i].tags.push("Nyhet");
+          }
 
-					// round to three decimal places
-					json[i].apk = Math.round((apk + Number.EPSILON) * 1000) / 1000;
-				}
+          // round to three decimal places
+          json[i].apk = Math.round((apk + Number.EPSILON) * 1000) / 1000;
+        }
 
-				json.sort((a, b) => b.apk - a.apk);
+        json.sort((a, b) => b.apk - a.apk);
 
-				fs.writeFile("_data/apk.yaml", yaml.dump(json), (error) => {
-					if (error) {
-						core.setFailed(error.message);
-					} else {
-						console.log("Build: Successfully wrote apk.yaml");
-					}
-				});
-			} catch (error) {
-				core.setFailed(error.message);
-			}
-		});
-	})
-	.on("error", (error) => {
-		core.setFailed(error.message);
-	});
+        fs.writeFile("_data/apk.yaml", yaml.dump(json), (error) => {
+          if (error) {
+            core.setFailed(error.message);
+          } else {
+            console.log("Build: Successfully wrote apk.yaml");
+          }
+        });
+      } catch (error) {
+        core.setFailed(error.message);
+      }
+    });
+  })
+  .on("error", (error) => {
+    core.setFailed(error.message);
+  });
